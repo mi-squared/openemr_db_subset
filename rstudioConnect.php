8302a466906ec187faf6e0ba228469685ddd9749
Daniel Pflieger <daniel@growlingflea.com>
2023-06-28 15:40:42 -0700

    finishing final touches

--- rstudioConnect.php
+++ rstudioConnect.php
@@ -14,21 +14,21 @@
  *  After the database and tables are created, we check for the user named 'rstudio'.  If the user does not exist, create it.
  *  Once created we make sure that 'rstudio' only has read access to the anayltics database
 
-        CREATE USER 'analytics'@'localhost' IDENTIFIED BY 'mi2AnalyticUser';
-        -- Grant read-only access to openemr database
-        GRANT SELECT ON openemr.* TO 'analytics'@'localhost';
-        GRANT RELOAD ON *.* TO 'analytics'@'localhost';
+CREATE USER 'analytics'@'localhost' IDENTIFIED BY 'mi2AnalyticUser';
+-- Grant read-only access to openemr database
+GRANT SELECT ON openemr.* TO 'analytics'@'localhost';
+GRANT RELOAD ON *.* TO 'analytics'@'localhost';
 
-       Create database analytics;
-        -- Grant full privileges on analytics databases
-        GRANT ALL PRIVILEGES ON `analytics\_%`.* TO 'analytics'@'localhost';
-       GRANT SELECT ON mysql.db TO 'analytics'@'localhost';
-       GRANT CREATE USER ON analytics.* TO 'analytics'@'localhost';
-        GRANT GRANT OPTION ON analytics.* TO 'analytics'@'localhost';
+Create database analytics;
+-- Grant full privileges on analytics databases
+GRANT ALL PRIVILEGES ON `analytics\_%`.* TO 'analytics'@'localhost';
+GRANT SELECT ON mysql.db TO 'analytics'@'localhost';
+GRANT CREATE USER ON analytics.* TO 'analytics'@'localhost';
+GRANT GRANT OPTION ON analytics.* TO 'analytics'@'localhost';
 FLUSH PRIVILEGES;
  *
  */
-
+$test = true; //Will limit all queries to the first 100.
 
 
 //the mySQL user that runs this script will be called analytics.  This user is creating a
@@ -36,7 +36,7 @@ $user = 'analytics';
 $password = 'mi2AnalyticUser';
 $host = 'localhost';
 $targetDatabase=  'analytics';
-$sourceDatabase = 'openemr';
+$sourceDatabase = 'peds_700';
 
 //create a connection as root to the instance of mySQL
 $conn = new mysqli($host, $user, $password);
@@ -45,7 +45,7 @@ $conn = new mysqli($host, $user, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connection to mySQL server successful as user: $user \nconnected to database: $targetDatabase\n";
+    echo date('Y-m-d h:i:s') . "\nConnection to mySQL server successful as user: $user \nconnected to database: $targetDatabase  \n";
 }
 
 //Here we do all the permission stuff
@@ -65,7 +65,9 @@ $createPatientDataTable = "CREATE TABLE patient_data (
 
 //get the pid, dob from patient_data.
 $selectPatientDataQuery = "select pid, dob from $sourceDatabase.patient_data ";
-
+if($test){
+    $selectPatientDataQuery .= " limit 100";
+}
 
 
 // Execute the query
@@ -104,37 +106,37 @@ if ($result->num_rows > 0) {
     $result = $conn->query($createUser);
     echo "User 'rstudio' created successfully. \n";
 }
-    //set permissions for rstudio client to only have read-only permissions of the analytic database
-    $result = $conn -> query("Use $targetDatabase"); //Logged in as
+//set permissions for rstudio client to only have read-only permissions of the analytic database
+$result = $conn -> query("Use $targetDatabase"); //Logged in as
 
-    //create the table using the query
-    if ($conn->query($createPatientDataTable) === TRUE) {
-        echo "Table 'patient_data' created successfully. \n";
+//create the table using the query
+if ($conn->query($createPatientDataTable) === TRUE) {
+    echo "Table 'patient_data' created successfully. \n";
 
-    } else {
-        die( "Error creating table: " . $conn->error . "\n\n");
+} else {
+    die( "Error creating table: " . $conn->error . "\n\n");
+}
+
+//user analytic queries the source database for patient data
+$result = $conn->query($selectPatientDataQuery);
+if ($result->num_rows > 0) {
+    echo("Inserting data into patient_data... \n");
+    $stmt = $conn->prepare("INSERT INTO $targetDatabase.patient_data (pid, dob) VALUES (?, ?)");
+    $stmt->bind_param("is", $pid, $dob);
+
+    // Process each row of data from OpenEMR and insert into Analytics
+    while ($row = $result->fetch_assoc()) {
+        $pid = $row["pid"];
+        $dob = $row["dob"];
+        $stmt->execute();
     }
 
-    //user analytic queries the source database for patient data
-    $result = $conn->query($selectPatientDataQuery);
-    if ($result->num_rows > 0) {
-        echo("Inserting data into patient_data... \n");
-        $stmt = $conn->prepare("INSERT INTO $targetDatabase.patient_data (pid, dob) VALUES (?, ?)");
-        $stmt->bind_param("is", $pid, $dob);
+    // Close the statement
+    $stmt->close();
 
-        // Process each row of data from OpenEMR and insert into Analytics
-        while ($row = $result->fetch_assoc()) {
-            $pid = $row["pid"];
-            $dob = $row["dob"];
-            $stmt->execute();
-        }
-
-        // Close the statement
-        $stmt->close();
-
-    } else {
-        die( "Error getting data: " . $conn->error . "\n\n");
-    }
+} else {
+    die( "Error getting data: " . $conn->error . "\n\n");
+}
 
 
 
@@ -152,12 +154,12 @@ if ($result) {
 
 //handle the vitals!
 
-$tableNames = ['form_vitals', 'form_encounter',  'form_observation'];
+$tableNames = [ 'form_vitals', 'form_encounter', 'form_observation', 'form_questionnaire_assessments'];
 foreach($tableNames as $tableName){
     $result = $conn->query("SHOW COLUMNS FROM $sourceDatabase.$tableName");
 
     if (!$result) {
-        echo("Error retrieving column information: " . $conn->error . '\n\n');
+        echo("Error retrieving column information: " . $conn->error . "\n\n");
         continue;
     }
 
@@ -185,14 +187,18 @@ foreach($tableNames as $tableName){
         echo("Created Table $targetDatabase \n");
     }
     $selectQuery = "SELECT * FROM $sourceDatabase.$tableName";
+    if($test) {
 
+        $selectQuery .= " Limit 100";
+    }
 // Execute the select query
     $result = $conn->query($selectQuery);
 
 // Check if the select query was successful
     if ($result) {
+        echo "Inserting $tableName \n";
         $insertQuery = "INSERT INTO `$targetDatabase`.`$tableName` VALUES (";
-        for ($i = 0; $i < 28; $i++) {
+        for ($i = 0; $i < $result->field_count; $i++) {
             $insertQuery .= ($i > 0 ? ", " : "") . "?";
         }
         $insertQuery .= ")";
@@ -202,7 +208,7 @@ foreach($tableNames as $tableName){
             // Bind the values to the prepared statement parameters dynamically
             $params = array_values($row);
             $types = str_repeat("s", count($params));
-            $insertStmt->bind_param($types, $params);
+            $insertStmt->bind_param($types, ...$params);
 
             // Execute the prepared statement
             $insertResult = $insertStmt->execute();
